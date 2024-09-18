7b3b87437cea6d2b2556ecfd095cbf484337d770
Swetank Mohanty <swetank.mohanty@outlook.com>
2024-09-18 15:28:49 +0530

    Added pages for front-end Angular application

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+
+import { DoctorsModule } from './pages/doctors/doctors.module';
+import { PatientsModule } from './pages/patients/patients.module';
+import { PharmacyModule } from './pages/pharmacy/pharmacy.module';
+import { BillingModule } from './pages/billing/billing.module';
+import { PaymentsModule } from './pages/payments/payments.module';
+import { AdminCentreModule } from './pages/admin-centre/admin-centre.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    DoctorsModule,
+    PatientsModule,
+    PharmacyModule,
+    BillingModule,
+    PaymentsModule,
+    AdminCentreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
